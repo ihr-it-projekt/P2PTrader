4fe99dee650e756b58dcd703ade576c23342c5b2
Ralf <ralfge@ihr-it-projekt.de>
2020-03-18 21:30:18 +0100

    add date object add manager and listener

---
+++ scripts/5_Mission/UI/P2PTraderItemMoveListener.c
@@ -0,0 +1,115 @@
+class P2PTraderItemMoveListener extends Managed
+{
+	ButtonWidget buttonSourceToTarget;
+	ButtonWidget buttonTargetToSource;
+	TextListboxWidget source;
+	TextListboxWidget target;
+	ref P2PTraderPreviewWindow previewWindow;
+	bool move;
+	
+	void P2PTraderItemMoveListener(ButtonWidget buttonSourceToTarget, ButtonWidget buttonTargetToSource, TextListboxWidget source, TextListboxWidget target, bool move, ItemPreviewWidget peviewWidget, MultilineTextWidget description, P2PItemService itemService) {
+		DebugMessageP2PTrader("Create Move listener: " + buttonSourceToTarget.GetName());
+		this.buttonSourceToTarget = buttonSourceToTarget;
+		this.buttonTargetToSource = buttonTargetToSource;
+		this.source = source;
+		this.target = target;
+		this.move = move;
+		previewWindow = new P2PTraderPreviewWindow(peviewWidget, description,itemService);
+		WidgetEventHandler.GetInstance().RegisterOnMouseButtonDown(buttonSourceToTarget, this, "MoveSourceToTarget");
+		WidgetEventHandler.GetInstance().RegisterOnMouseButtonDown(buttonTargetToSource, this, "MoveTargetToSource");
+		
+		WidgetEventHandler.GetInstance().RegisterOnMouseButtonUp(source, this, "GetSourceItem");
+		WidgetEventHandler.GetInstance().RegisterOnMouseButtonUp(target, this, "GetTargetItem");
+	}
+	
+	bool MoveSourceToTarget(Widget w, int x, int y, int button) {
+		MoveItemFromListWidgetToListWidget(source, target, buttonSourceToTarget, move);
+		
+		return false;
+	}
+	
+	bool MoveTargetToSource(Widget w, int x, int y, int button) {
+		MoveItemFromListWidgetToListWidget(target, source, buttonTargetToSource, true);
+		
+		return false;
+	}	
+	
+	bool GetSourceItem(Widget w, int x, int y, int button) {
+		P2PTraderItem item = CheckButton(source, buttonSourceToTarget);
+		previewWindow.UpdatePreview(item);
+		
+		return false;
+	}
+	
+	bool GetTargetItem(Widget w, int x, int y, int button) {
+		P2PTraderItem item = CheckButton(target, buttonTargetToSource);
+		previewWindow.UpdatePreview(item);
+		return false;
+	}
+	
+	private P2PTraderItem CheckButton(TextListboxWidget widget, ButtonWidget button) {
+		DebugMessageP2PTrader("Click on " + widget.GetName());
+		int pos = widget.GetSelectedRow();
+		
+		if (pos == -1) {
+			return null;
+		}
+		P2PTraderItem item;
+		widget.GetItemData(pos, 0, item);
+		
+		
+		button.Show(false);
+		if (item) {
+			button.Show(true);
+			return item;
+		}
+		
+		return null;
+	}
+	
+	private void MoveItemFromListWidgetToListWidget(TextListboxWidget sourceWidget, TextListboxWidget targetWidget, ButtonWidget button, bool mustMove) {
+		DebugMessageP2PTrader("Move item from: " + sourceWidget.GetName() + " to: " + targetWidget.GetName());
+		int pos = sourceWidget.GetSelectedRow();
+		if (pos == -1) {
+			DebugMessageP2PTrader("Pos is not valid!");
+			return;
+		}
+		P2PTraderItem item;
+		sourceWidget.GetItemData(pos, 0, item);
+		
+		if (item) {
+			if (targetWidget) {
+				DebugMessageP2PTrader("Add item to target");
+				targetWidget.AddItem(item.translatedName, item, 0);
+			}
+			
+			if (mustMove) {
+				DebugMessageP2PTrader("Remove item from source!");
+				sourceWidget.RemoveRow(pos);
+				
+				int countItems = sourceWidget.GetNumItems();
+				
+				int newItemNumber =  pos;
+				if (countItems == 0) {
+					newItemNumber = -1;
+					button.Show(false);
+				} else if(newItemNumber >= countItems) {
+					newItemNumber = countItems - 1;
+				} else if(countItems == 1) {
+					newItemNumber = 0;
+				} 
+				
+				sourceWidget.SelectRow(newItemNumber);
+				
+				if (-1 != newItemNumber) {
+					item = CheckButton(sourceWidget, button);
+					previewWindow.UpdatePreview(item);
+				}
+			}
+		} else {
+			DebugMessageP2PTrader("No item found to move!");
+			button.Show(false);
+		}
+		
+	}
+}
