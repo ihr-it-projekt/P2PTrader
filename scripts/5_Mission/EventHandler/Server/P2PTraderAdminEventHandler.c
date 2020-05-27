55a719fd5063086f6e3e8c6d3ae5344269136e62
Ralf Geisthardtz <RalfGe@gmail.com>
2020-05-27 17:10:51 +0200

    add admin event handler

---
+++ scripts/5_Mission/EventHandler/Server/P2PTraderAdminEventHandler.c
@@ -0,0 +1,41 @@
+class P2PTraderAdminEventHandler
+{
+	private ref P2PTraderPlayerInventory inventory;
+	private P2PTraderStock traderStock;
+	private P2PTraderConfigParams config;
+	
+    void P2PTraderAdminEventHandler(P2PTraderStock traderStock, P2PTraderConfigParams config) {
+		this.traderStock = traderStock;
+        DebugMessageP2PTrader("Register P2PTraderAdminEventHandler");
+        inventory = new P2PTraderPlayerInventory;
+		this.config = config;
+		GetDayZGame().Event_OnRPC.Insert(HandleEvents);
+    }
+
+    void ~P2PTraderAdminEventHandler() {
+        GetDayZGame().Event_OnRPC.Remove(HandleEvents);
+    }
+
+    void HandleEvents(PlayerIdentity sender, Object target, int rpc_type, ParamsReadContext ctx) {
+        if (!IsServerP2PTrader()) {
+            return;
+        }
+
+        if (rpc_type == P2P_TRADER_EVENT_ADMIN_DELETE_OFFER) {
+			DebugMessageP2PTrader("receive P2P_TRADER_EVENT_ADMIN_DELETE_OFFER");
+            Param2<DayZPlayer, int> parameterOffer;
+            if (ctx.Read(parameterOffer)) {
+				DayZPlayer player = parameterOffer.param1;
+				int offerId = parameterOffer.param2;
+				
+				if (config.IsAdmin(player)) {
+					P2PTraderPlayerMarketOffer offer = traderStock.GetPlayerToMarketOfferById(offerId);
+					
+					traderStock.RemovePlayerToMarketOffer(offer);
+					GetGame().RPCSingleParam(player, P2P_TRADER_EVENT_ADMIN_DELETE_OFFER_RESPONSE, new Param1<string>("#deleted"), true, player.GetIdentity());
+                	DebugMessageP2PTrader("send P2P_TRADER_EVENT_ADMIN_DELETE_OFFER_RESPONSE to player");				
+				}
+            }
+        }
+    }
+}
